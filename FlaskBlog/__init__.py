ef681b503770128813e753ca99a51206fc2b7969
Hipengpeng <com.zhang.peng.peng@gmail.com>
2019-11-18 21:47:51 +1000

    add the database

--- FlaskBlog/__init__.py
+++ FlaskBlog/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY']='1112015112151f5rgg15dsf'
 app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
-
+bcrypt = Bcrypt(app)
 from FlaskBlog import router
--- FlaskBlog/router.py
+++ FlaskBlog/router.py
@@ -1,7 +1,7 @@
 from flask import render_template, url_for,flash,redirect
-from FlaskBlog import app
+from FlaskBlog import app,db,bcrypt
 from FlaskBlog.forms import RegisterForm, LoginForm
-
+from FlaskBlog.models import User
 posts = [
     {
         'author': 'peng',
@@ -33,6 +33,11 @@ def about():
 def register():
     form = RegisterForm()
     if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username = form.username.data,email=form.email.data, password=hashed_password)
+        db.create_all()
+        db.session.add(user)
+        db.session.commit()
         flash(f'Account created for {form.username.data}','success')
         return redirect(url_for('home'))
     return render_template('register.html',title='Register',form=form)

23109442ebb786a427935b18aace512c1d0c0156
Hipengpeng <com.zhang.peng.peng@gmail.com>
2019-11-19 18:16:44 +1000

    add the database

--- FlaskBlog/__init__.py
+++ FlaskBlog/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY']='1112015112151f5rgg15dsf'
-app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.sqlite'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 from FlaskBlog import router
--- FlaskBlog/router.py
+++ FlaskBlog/router.py
@@ -2,6 +2,7 @@ from flask import render_template, url_for,flash,redirect
 from FlaskBlog import app,db,bcrypt
 from FlaskBlog.forms import RegisterForm, LoginForm
 from FlaskBlog.models import User
+from flask_login import login_user
 posts = [
     {
         'author': 'peng',
@@ -35,10 +36,11 @@ def register():
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
         user = User(username = form.username.data,email=form.email.data, password=hashed_password)
-        db.create_all()
         db.session.add(user)
         db.session.commit()
         flash(f'Account created for {form.username.data}','success')
+        user1 = User.query.all()
+        print(user1)
         return redirect(url_for('home'))
     return render_template('register.html',title='Register',form=form)
 
@@ -46,9 +48,10 @@ def register():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data== 'peng@peng.com' and form.password.data=='password':
-            flash("You have logged in!",'success')
-            return redirect(url_for('home'))
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password,form.password.data):
+           login_user(user,remember=form.remember.data)
+           return redirect(url_for('home'))
         else:
             flash("Login unsuccessfully. Please check username and password",'danger')
     return render_template('login.html',title='Login',form=form)
